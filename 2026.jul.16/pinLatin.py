af120a05dfa4797748af0551124980ddf1809acf
gganiku <gabriel_ganiku@hotmail.com>
2026-07-16 17:27:55 -0300

    feat: add way on words

--- 2026.jul.16/pinLatin.py
+++ 2026.jul.16/pinLatin.py
@@ -5,15 +5,28 @@ Given a string, convert it to Pig Latin using the following rules:
 - If a word begins with one or more consonants, move them to the end and add "ay". For example, "hello" converts to "ellohay".
 - Preserve the case of the first letter. For example, "Hello" converts to "Ellohay".
 '''
-
+s = input('Write a word: ')
 def pig_latin(s):
-    list = s.split()
+    # list = s.split()
+    ns = ''
+    # Considering that s is a single word
+    if s[0] in 'aeiou':
+        ns = f"{s}way"
+    else:
+        ns = "Error. The word doesn't beggins with an vowel."
+
+    # result = " ".join(ns)
+    return ns
+
+a = pig_latin(s)
+print(a)
 
 
-    if 'aeiou' in s[0]:
-        list = f'{s + 'way'}'
 
-    result = " ".join(list)
-    return result
+# s = input(str('Write a word: '))
+# # ns = f"{s}way"
+# result = ''
+# if s in 'aeiou':
+#     result = f"{s}way"
 
-print(pig_latin('universe'))
+# print(result)
