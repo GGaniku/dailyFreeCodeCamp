8f593fd1982b24eec88e2552016f28bc7112ceff
gganiku <gabriel_ganiku@hotmail.com>
2026-07-16 18:02:03 -0300

    feat: rule 2

--- 2026.jul.16/pinLatin.py
+++ 2026.jul.16/pinLatin.py
@@ -1,21 +1,29 @@
 ''' Challenge of July 16th, 2026
 Given a string, convert it to Pig Latin using the following rules:
 
-- If a word begins with a vowel ("a", "e", "i", "o", or "u"), add "way" to the end. For example, "universe" converts to "universeway".
-- If a word begins with one or more consonants, move them to the end and add "ay". For example, "hello" converts to "ellohay".
-- Preserve the case of the first letter. For example, "Hello" converts to "Ellohay".
+1) If a word begins with a vowel ("a", "e", "i", "o", or "u"), add "way" to the end. For example, "universe" converts to "universeway".
+2) If a word begins with one or more consonants, move them to the end and add "ay". For example, "hello" converts to "ellohay".
+3) Preserve the case of the first letter. For example, "Hello" converts to "Ellohay".
 '''
 s = input('Write a word: ')
 def pig_latin(s):
     res = ''
+    cons = 0
 
     # Considering that s is a single word
+
+    # Rule 1
     if s[0] in 'aeiou':
         res = f"{s}way"
-    elif s[0] not in 'aeiou':
-        res = f"{s[1:]}{s[:1]}ay"
+
+    # Rule 2
     else:
-        res = "Error. This word isn't allowed. Try again."
+        for letter in s:
+            if letter not in 'aeiou':
+                cons += 1
+            else:
+                break
+        res = f"{s[cons:]}{s[:cons]}"
 
     return res
 
